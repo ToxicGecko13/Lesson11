9bf82e8914724ab1d1aae4407feb94728f6793e3
Carsten Ruhl <ToxicGecko13@Gmail.com>
2018-12-10 15:48:54 -0800

    First Commit

---
+++ Mypages/main.js
@@ -0,0 +1,15 @@
+// document.querySelector('#row1col1').addEventListener('click', RowColClick);
+document.querySelector('#row1col2').addEventListener('click', RowColClick);
+// document.querySelector('#row1col3').addEventListener('click', RowColClick);
+// document.querySelector('#row2col1').addEventListener('click', RowColClick);
+// document.querySelector('#row2col2').addEventListener('click', RowColClick);
+// document.querySelector('#row2col3').addEventListener('click', RowColClick);
+// document.querySelector('#row3col1').addEventListener('click', RowColClick);
+// document.querySelector('#row3col2').addEventListener('click', RowColClick);
+// document.querySelector('#row3col3').addEventListener('click', RowColClick);
+
+function RowColClick(event) {
+    document.getElementById('demo');
+    alert(event.target.id);
+    console.log(event);
+}
